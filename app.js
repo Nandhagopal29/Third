f901d706856173a7e1dc1c4e1ed1846d5961a635
Nandhagopal29 <116285953+Nandhagopal29@users.noreply.github.com>
2022-12-24 22:44:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -73,6 +73,8 @@ app.post("/failure",function (req,res) {
     res.redirect("/")
 })
 
-app.listen(3000,function () {
-    console.log("Server started running on 3000.");
+const port= process.env.PORT || 3000;
+
+app.listen(port,function () {
+    console.log('Server started running on ${port}');
 })
